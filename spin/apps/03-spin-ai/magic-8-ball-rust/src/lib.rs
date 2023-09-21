b42f551fdbe41058bbc7e99fea1317c6abba8d02
Mikkel Mørk Hegnhøj <mikkel@fermyon.com>
2023-09-21 12:17:49 +0200

    Update magic-8-ball-rust app
    Signed-off-by: Mikkel Mørk Hegnhøj <mikkel@fermyon.com>

--- spin/apps/03-spin-ai/magic-8-ball-rust/src/lib.rs
+++ spin/apps/03-spin-ai/magic-8-ball-rust/src/lib.rs
@@ -15,29 +15,47 @@ fn handle_magic_8_ball(req: Request) -> Result<Response> {
             .header("Content-Type", "application/json")
             .body(Some("No question provided".into()))?);
     }
-    let answer_json = format!(r#"{{"answer": "{}"}}"#, answer(question)?);
+    let answer_json = format!(r#"{{"answer": "{}"}}"#, answer(question.to_string())?);
     Ok(http::Response::builder()
         .status(200)
         .header("Content-Type", "application/json")
         .body(Some(answer_json.into()))?)
 }
 
-fn answer(question: &str) -> Result<String> {
+fn answer(mut question: String) -> Result<String> {
+    if &question[question.len() - 1..] != "?" {
+        question.push('?');
+    }
+    println!("Question: {:?}", question);
     let prompt = format!(
-        r"<<SYS>>You are acting as an omniscient Magic 8 Ball that answers users' yes or no questions.<</SYS>>
-        [INST]Answer the question that follows the 'User:' prompt with a short response. Prefix your response with 'Answer:'.
-        If the question is not a yes or no question, reply with 'I can only answer yes or no questions'.
-        Your tone should be expressive yet polite. Always restrict your answers to 10 words or less. 
-        NEVER continue a prompt by generating a user question.[/INST]
-        User: {question}"
+        r"<s>[INST] <<SYS>>
+        You are acting as a Magic 8 Ball that answers questions about whether a person should do a thing or not.
+        If the question is not about a decision the person has to make, reply with 'I cannot answer that question'
+        Your tone should be expressive yet polite.
+        Your answers should be 10 words or less.
+        Prefix your response with 'Answer:'.
+        <</SYS>>
+        {question}[/INST]"
     );
-
     // Set model to default Llama2 or the one configured in runtime-config.toml
     let model = llm::InferencingModel::Llama2Chat;
-    let answer = llm::infer(model, &prompt)?.text;
+    let answer = llm::infer_with_options(
+        model,
+        &prompt,
+        llm::InferencingParams {
+            max_tokens: 20,
+            repeat_penalty: 1.5,
+            repeat_penalty_last_n_token_count: 20,
+            temperature: 0.25,
+            top_k: 5,
+            top_p: 0.25,
+        },
+    )?
+    .text;
     let mut answer = answer.trim();
     while let Some(a) = answer.strip_prefix("Answer:") {
         answer = a.trim();
     }
+    println!("Answer: {:?}", answer);
     Ok(answer.trim().to_string())
 }
