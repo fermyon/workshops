f0548ec4bd3f03ca2b3e460b5d50650b9db6c724
Justin Pflueger <justin.pflueger@fermyon.com>
2024-03-25 13:46:15 -0600

    update part 3 to follow same format as part 4

--- magic-8-ball/apps/03-spin-ai/magic-eight-ball-python/app.py
+++ magic-8-ball/apps/03-spin-ai/magic-eight-ball-python/app.py
@@ -1,13 +1,44 @@
+import json
+from textwrap import dedent
 from spin_sdk import http, llm
 from spin_sdk.http import Request, Response
 
 class IncomingHandler(http.IncomingHandler):
     def handle_request(self, request: Request) -> Response:
-        opts = llm.InferencingParams(20, 1.5, 20, 0.25, 5, 0.25)
-        prompt = "You are a Magic 8 Ball that predicts the answer to a question about events now or in the future. Your tone should be expressive yet polite. Your answers should be 10 words or less. Prefix your response with Answer:"
-        res = llm.infer_with_options("llama2-chat", prompt, options = opts)
+        question=str(request.body, "utf-8")
+        if not question:
+            return Response(
+                400,
+                {"content-type": "text/plain"},
+                bytes("No question provided", "utf-8")
+            )
+        answer_json = json.dumps({
+            "answer": answer(question)
+        })
         return Response(
             200,
-            {"content-type": "text/plain"},
-            bytes(res.text, "utf-8")
+            {"content-type": "application/json"},
+            bytes(answer_json, "utf-8")
         )
+
+def answer(question: str):
+    print("Question:", question)
+    prompt = dedent("""\
+        <s>[INST] <<SYS>>
+        You are acting as a Magic 8 Ball that predicts the answer to a questions about events now or in the future.
+        Your tone should be expressive yet polite.
+        Your answers should be 10 words or less.
+        <</SYS>>
+        {}[/INST]
+    """).format(question)
+    opts = llm.InferencingParams(
+        max_tokens=20,
+        repeat_penalty=1.5,
+        repeat_penalty_last_n_token_count=20,
+        temperature=0.25,
+        top_k=5,
+        top_p=0.25,
+    )
+    answer = llm.infer_with_options("llama2-chat", prompt, opts).text.strip()
+    print("Answer:", answer)
+    return answer
